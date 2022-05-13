769d296c1eeed4bad0425af49fe755a3a9e17fe2
lin <k-lin@ttc-net.co.jp>
2022-05-13 01:41:45 +0000

    Connnect to MySQL

--- routes/todos.js
+++ routes/todos.js
@@ -1,11 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET todos listing. */
-router.get('/', function(req, res, next) {
-	  res.send('respond with a resource');
-});
-
 /* POST create todo. */
 router.post('/', function(req, res, next) {
 	  const title = req.body.title;
@@ -16,6 +11,27 @@ router.post('/', function(req, res, next) {
 /* GET show todo. */
 router.get('/:id', function(req, res, next) {
 	  res.send(`respond with a resource id ${req.params.id}`);
+});
+
+/* GET todos listing. */
+router.get('/', function(req, res, next) {
+	  const mysql = require('mysql');
+	  const connection = mysql.createConnection({
+		      host: 'localhost',
+		      user: 'my_node_app',
+		      password: 'my_node_app',
+		      database: 'my_node_app'
+		    });
+	  
+	  connection.connect();
+	  
+	  connection.query('SELECT 1 + 1 AS solution', (err, rows, fields) => {
+		      if (err) throw err;
+		    
+		      res.send(`The solution is: ${rows[0].solution}`);
+		    })
+	  
+	  connection.end();
 });
 
 module.exports = router;
