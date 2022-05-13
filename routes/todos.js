75277a12b68696b5750a44de59d41e37b65155a0
lin <k-lin@ttc-net.co.jp>
2022-05-13 02:05:41 +0000

    Add creating todo

--- routes/todos.js
+++ routes/todos.js
@@ -1,13 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-/* POST create todo. */
-router.post('/', function(req, res, next) {
-	  const title = req.body.title;
-	  const content = req.body.content;
-	  res.send(`respond with a new resource ${title} ${content}`);
-});
-
 /* GET show todo. */
 router.get('/:id', function(req, res, next) {
 	  res.send(`respond with a resource id ${req.params.id}`);
@@ -31,6 +24,33 @@ router.get('/', function(req, res, next) {
 		      res.send(`The solution is: ${rows[0].solution}`);
 		    })
 	  
+	  connection.end();
+});
+
+/* POST create todo. */
+router.post('/', function(req, res, next) {
+	  const title = req.body.title;
+	  const content = req.body.content;
+	  const mysql = require('mysql');
+	  const connection = mysql.createConnection({
+		      host: 'localhost',
+		      user: 'my_node_app',
+		      password: 'my_node_app',
+		      database: 'my_node_app'
+		    });
+	  
+	  connection.connect();
+
+	  const query = 'INSERT INTO `todos` (`title`, `content`, `created_at`, `updated_at`)' + 
+		    'VALUES (?, ?, SYSDATE(), SYSDATE())';
+	  connection.query(query,
+		      [title, content],
+		      (err, rows, fields) => {
+			            if (err) throw err;
+
+			            res.send(`respond with a new resource ${title} ${content}`);
+			        });
+
 	  connection.end();
 });
 
